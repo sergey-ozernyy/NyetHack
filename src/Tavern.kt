cb9267779660f164cd88b0f7a0dbd485efb23779
sergey <sergey.ozernyy@mail.ru>
2020-06-20 11:40:30 +0300

    Все задания до "Преобразование Int в Double" из 8 главы

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -1,19 +1,36 @@
 const val TAVERN_NAME = "Tearnyl's Folly"
+var playerGold = 10
+var playerSilver = 10
+
+
 
 fun main(args: Array<String>) {
 
     placeOrder("shandy,Dragon's Breath,5.91")
+    //placeOrder("elixir,Shirley's Temple,4.12")
 
 }
 
+fun performPurchase(price: Double){
+    displayBalace()
+    println("Purchasing item for $price")
+}
+
+private fun displayBalace(){
+    println("Player's purse balance: Gold: $playerGold , Silver: $playerSilver")
+}
+
 private fun toDragonSpeak(phrase: String) =
-        phrase.replace(Regex("[aeiou]")){
+        phrase.replace(Regex("[aAeEiIoOuU]")){
             when (it.value) {
                 "a" -> "4"
+                "A" -> "4"
                 "e" -> "3"
                 "i" -> "1"
+                "I" -> "1"
                 "o" -> "0"
                 "u" -> "|_|"
+                "U" -> "|_|"
                 else -> it.value
             }
         }
@@ -28,6 +45,12 @@ fun placeOrder(menuData:String){
     val message = "Madrigal buys a $name ($type) for $price."
     println(message)
 
-    val phrase = "Ah, delicions $name!"
-    println("Madrigal exclaims: ${toDragonSpeak(phrase)}")
+    performPurchase(price.toDouble())
+
+    val phrase = if (name == "Dragon's Breath") {
+        "Madrigal exclaims: ${toDragonSpeak(("Ah, delicions $name!"))}"
+    } else {
+        "Madrigal says: Thanks for the $name."
+    }
+    println(phrase)
 }
