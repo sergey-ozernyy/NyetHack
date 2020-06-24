11deb89563b9d31f7a62e3bb2fa8a9fbb349a9c5
sergey <sergey.ozernyy@mail.ru>
2020-06-24 19:17:24 +0300

    Все задания, кроме "Реализация карты мира" и "Позвонить в колокол", до "Реализация по умолчанию", 16 главы "Интерфейсы"

--- src/com/bignerdranch/nyethack/Game.kt
+++ src/com/bignerdranch/nyethack/Game.kt
@@ -1,27 +1,88 @@
 package com.bignerdranch.nyethack
 
+import java.lang.IllegalStateException
+import kotlin.system.exitProcess
+
 fun main (args: Array<String>){
-    val player = Player("Madrigal")
-    player.castFireball()
 
-    val auraColor = player.auraColor()
-    //Состояние игрока
-    printPlayerStatus(player)
-
-    player.auraColor()
-
+    Game.play()
 
 }
 
-private fun printPlayerStatus(player: Player) {
-    println(
-        "(Aura: ${player.auraColor()}) " +
-                "(Blassed: ${if (player.isBlessed) "YES" else "NO"})"
+
+object Game {
+    private val player = Player("Madrigal")
+    private var currentRoom : Room = TownSquare()
+
+    private var worldMap = listOf(
+            listOf(currentRoom, Room("Tavern"), Room("Back Room")),
+            listOf(Room("Long Corridor"), Room("Generic Room"))
     )
-    println("${player.name} ${player.formatHealthStatus()}")
+
+    init {
+        println("Welcome, adventurer.")
+        player.castFireball()
+    }
+    fun play(state:Boolean = true){
+        while (state){
+            println(currentRoom.description())
+            println(currentRoom.load())
+            //Состояние игрока
+            printPlayerStatus(player)
+
+            print("> Enter your command: ")
+            println(GameInput(readLine()).processCommand())
+        }
+        println("Goodbye!")
+        exitProcess(-1)
+
+    }
+
+    private fun printPlayerStatus(player: Player) {
+        println(
+                "(Aura: ${player.auraColor()}) " +
+                        "(Blassed: ${if (player.isBlessed) "YES" else "NO"})"
+        )
+        println("${player.name} ${player.formatHealthStatus()}")
+    }
+
+    private class GameInput(arg:String?){
+        private val input = arg ?: ""
+        val command = input.split(" ")[0]
+        val argument = input.split(" ")
+                .getOrElse(1,{""})
+
+        fun processCommand() = when (command.toLowerCase()){
+            "move" -> move(argument)
+            "quit" -> play(false)
+            "map" -> map()
+            else -> commandNotFound()
+        }
+
+        private fun commandNotFound() = "I'm not quite sure what you're trying to do!"
+
+
+    }
+
+    private fun move(directionInput: String) =
+        try {
+            val direction = Direction.valueOf(directionInput.toUpperCase())
+            val newPosition = direction.updateCoordinate(player.currentPosition)
+            if (!newPosition.isBounds){
+                throw IllegalStateException("$direction is out of bounds.")
+            }
+            val newRoom = worldMap[newPosition.y][newPosition.x]
+            currentRoom = newRoom
+            "OK, you move $direction to the ${newRoom.name}.\n${newRoom.load()}"
+        } catch (e:Exception) {
+            "Invalid direction: $directionInput"
+        }
+
+    private fun map(){
+        println("${player.currentPosition}")
+    }
+
 }
-
-
 
 
 
--- src/com/bignerdranch/nyethack/Player.kt
+++ src/com/bignerdranch/nyethack/Player.kt
@@ -3,9 +3,22 @@ import java.io.File
 
 
 class Player(_name:String,
-             var healthPoints:Int = 100,
+             override var healthPoints:Int = 100,
              val isBlessed:Boolean,
-             private val isImmortal:Boolean) {
+             private val isImmortal:Boolean):Fightable {
+    override val diceCount: Int = 3
+    override val diceSides: Int = 6
+
+    override fun attack(opponent: Fightable): Int {
+        val damageDealt = if (isBlessed) {
+            damageRoll * 2
+        } else {
+            damageRoll
+        }
+        opponent.healthPoints -= damageDealt
+        return damageDealt
+    }
+
     var name = _name
         get() = "${field.capitalize()} of ${homeTown}"
         private set(value) {
@@ -13,6 +26,7 @@ class Player(_name:String,
         }
 
     val homeTown by lazy{selectHomeTown()}
+    var currentPosition = Coordinate(0,0)
 
     init {
         require(healthPoints > 0, {"healthPoints must be greater then zero."})
